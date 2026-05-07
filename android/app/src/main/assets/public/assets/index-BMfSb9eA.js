7a24d4730233c7b865493400a5a94eea0f72a1f3
semorrecualg <semorr@gmail.com>
2026-05-07 16:48:52 -0300

    feat: Configure Android project for Capacitor V3
    Update Android build configurations, dependencies, and ignore files to support Capacitor V3 integration. Includes changes to CI workflow for Android builds.

--- android/app/src/main/assets/public/assets/index-BMfSb9eA.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Clo03EP1.js","./index-CQ02Jwg6.js","./index-C9_M2WSx.css"])))=>i.map(i=>d[i]);
-import{r,_ as p}from"./index-CQ02Jwg6.js";const o=r("App",{web:()=>p(()=>import("./web-Clo03EP1.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
---
+++ android/app/src/main/java/com/auditoriainteligente/v3/MainActivity.java
@@ -0,0 +1,5 @@
+package com.auditoriainteligente.v3;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
