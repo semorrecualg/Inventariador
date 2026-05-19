7bd35eb6f76e93c028f77233cd4580c6c0228499
semorrecualg <semorr@gmail.com>
2026-05-19 11:05:51 -0300

    feat: Update Capacitor app ID and name
    Changes the application ID from 'com.gbr.inventario.expert' to 'com.gbr.kardek' and updates the application name to 'GBR KARDEK' in the capacitor configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.gbr.inventario.expert',
+  appId: 'com.gbr.kardek',
   appName: 'GBR KARDEK',
   webDir: 'dist',
   server: {
