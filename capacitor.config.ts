0f2c7356d155c372b02447b40feddc18caab392f
semorrecualg <semorr@gmail.com>
2026-05-19 10:13:43 -0300

    feat: Update app name and version
    Renames the application from 'Inventário Expert' to 'GBR KARDEK' and updates the version number to reflect significant changes and preparation for a new release.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gbr.inventario.expert',
-  appName: 'Inventário Expert',
+  appName: 'GBR KARDEK',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
