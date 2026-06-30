3a37214742b89d04440ee49b0dbbabe6e33aace9
semorrecualg <semorr@gmail.com>
2026-06-30 15:47:13 +0000

    feat(sre): corrigir divergencias sonarlint e blindar guardiao de rotas

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,17 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  readonly husky_skip_init=1
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-  # Avoid any interactive prompt
-  export GIT_TERMINAL_PROMPT=0
-
-  husky_env() {
-    if [ -f "$1" ]; then
-      . "$1"
-    fi
-  }
-
-  husky_env "$XDG_CONFIG_HOME/husky/init.sh"
-  husky_env "$HOME/.config/husky/init.sh"
-  husky_env "$HOME/.huskyrc"
-fi
+They WILL FAIL in v10.0.0
+"
